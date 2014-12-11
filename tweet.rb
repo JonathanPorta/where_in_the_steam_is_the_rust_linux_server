32f65d1abc8ae4c75c82ab4842bbb64f51a04c59
Jonathan Porta <jonathan@jonathanporta.com>
2014-12-10 18:48:55 -0800

    Typo...

--- tweet.rb
+++ tweet.rb
@@ -11,7 +11,7 @@ end
 
 status_message = ARGV[0]
 is_pull_request = ENV['TRAVIS_PULL_REQUEST']
-build_url = "https://travis-ci.org/#{ TRAVIS_REPO_SLUG }/builds/#{ TRAVIS_BUILD_ID }"
+build_url = "https://travis-ci.org/#{ ENV['TRAVIS_REPO_SLUG'] }/builds/#{ ENV['TRAVIS_BUILD_ID'] }"
 
 # Don't send empty tweets, or tweets for PRs.
 client.update("#{ status_message } #{ build_url }") if status_message && !is_pull_request
